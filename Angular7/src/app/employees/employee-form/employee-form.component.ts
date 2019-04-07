8851a6d416ab8cf4527a0d84a0ed81c2a991a6ee
Pratik <pratik_238@hotmail.com>
2019-04-07 17:36:43 +0530

    First Ang 7 with insert_update_Delete

---
+++ Angular7/src/app/employees/employee-form/employee-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/shared/employee.service';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-employee-form',
+  templateUrl: './employee-form.component.html',
+  styleUrls: ['./employee-form.component.css']
+})
+export class EmployeeFormComponent implements OnInit {
+  constructor(private service: EmployeeService, private toastr: ToastrService) { }
+  //********************
+  ngOnInit() {
+    this.resetForm();
+  }
+  //********************
+  resetForm(form?: NgForm) {
+    if (form != null) {
+      form.resetForm();
+    }
+    this.service.formData = {
+      EmployeeID: null,
+      FullName: '',
+      EMPCode: '',
+      Mobile: '',
+      Position: ''
+    }
+  }
+  //********************
+  onSubmit(form: NgForm) {
+    if (form.value.EmployeeID == null) {
+      this.inserRecord(form);
+    }
+    else {
+      this.updateRecord(form);
+    }
+  }
+  //********************
+  inserRecord(form: NgForm) {
+    this.service.postEmployee(form.value).subscribe(res => {
+      this.toastr.success('Inserted successfully', 'EMP. Register');
+      this.resetForm(form);
+      this.service.refreshlist();
+    });
+  }
+  updateRecord(form: NgForm) {
+    this.service.putEmployee(form.value).subscribe(res => {
+      this.toastr.info('Updated successfully', 'EMP. Register');
+      this.resetForm(form);
+      this.service.refreshlist();
+    });
+  }
+
+}
